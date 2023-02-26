136b13589a198aea09ee5332d58306588cc33167
jewelrana3 <juyelrana7752@gmail.com>
2023-02-26 08:59:15 +0600

    neme

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const phoneLoad = async(inputFluidString) =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputFluidString}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayLoad(data.data)
+
+}
+
+const displayLoad = phones =>{
+    const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.innerText='';
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col')
+        phoneDiv.innerHTML=`
+        <div class="card p-5">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+      </div>
+        `;
+        phoneContainer.appendChild(phoneDiv)
+    });
+}
+document.getElementById('btn-click').addEventListener('click',function(){
+    const inputFluid = document.getElementById('input-fluid');
+    const inputFluidString = inputFluid.value;
+    phoneLoad(inputFluidString)
+})
+
+phoneLoad();
